79725ad34728321dd13766a8ba46a20a66a19b62
--replace-all <qngolg@126.com>
2020-03-02 10:43:16 +0800

    001 rename the test method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,11 +8,11 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void should_update_quality_when_given_a_foo_item() {
         Item[] items = new Item[] { new Item("foo", 1, 5) };
         GildedRose app = new GildedRose(items);
         app.update_quality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("should_update_quality_when_given_items", app.items[0].name);
         assertThat(app.items[0].quality, is(4));
         assertThat(app.items[0].sellIn, is(0));
     }
